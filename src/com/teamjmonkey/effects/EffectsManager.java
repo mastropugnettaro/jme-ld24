f0d71ec5124afd812af7c78f4ce85734d665d9c5
wezrules@gmail.com <wezrules@gmail.com@1719d32a-b489-8db3-6133-feb022d4e5f0>
2012-08-22 22:10:13 +0000

    Empty Effects package

---
+++ src/com/teamjmonkey/effects/EffectsManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamjmonkey.effects;
+
+/**
+ *
+ * @author Wesley
+ */
+public class EffectsManager {
+
+}
