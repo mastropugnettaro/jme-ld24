d239d3e1c34443d370ee62662631c7f3961aa22f
terryr.rohr@gmail.com <terryr.rohr@gmail.com@1719d32a-b489-8db3-6133-feb022d4e5f0>
2012-08-26 13:01:53 +0000

    animation manager

---
+++ src/com/teamjmonkey/animation/AnimConf.java
@@ -0,0 +1,9 @@
+package com.teamjmonkey.animation;
+
+public class AnimConf {
+	
+	public final static Integer CHANNELS = 3;
+	public final static Integer HEAD = 0;
+	public final static Integer UPPER_BODY = 1;
+	public final static Integer LOWER_BODY = 2;
+}
---
+++ src/com/teamjmonkey/animation/AnimInfo.java
@@ -0,0 +1,21 @@
+package com.teamjmonkey.animation;
+
+import com.jme3.animation.LoopMode;
+
+public class AnimInfo {
+
+	public String name;
+	public float speed;
+	public float blendTime;
+	public LoopMode loop;
+	public Integer channelId;
+	
+	public AnimInfo(String name, float speed, float blendTime, LoopMode loop, Integer channelId){
+		this.name = name;
+		this.speed = speed;
+		this.blendTime = blendTime;
+		this.loop = loop;
+		this.channelId = channelId;
+	}
+	
+}
