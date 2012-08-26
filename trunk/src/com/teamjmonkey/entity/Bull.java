723ae337454395b6b4c6d6fba8eed34bca49c88f
wezrules@gmail.com <wezrules@gmail.com@1719d32a-b489-8db3-6133-feb022d4e5f0>
2012-08-26 18:51:15 +0000

    Fix level transition bullet error

--- trunk/src/com/teamjmonkey/entity/Bull.java
+++ trunk/src/com/teamjmonkey/entity/Bull.java
@@ -2,8 +2,6 @@ package com.teamjmonkey.entity;
 
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
-import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import com.teamjmonkey.animation.AnimComponent;
 import com.teamjmonkey.controls.AggroControl;
 import com.teamjmonkey.graphics.Graphics;
@@ -15,20 +13,6 @@ public class Bull extends MovableEntity {
         spatial.setName("enemy");
 
         animComponent = new AnimComponent(spatial);
-
-        //this to to align the controls better
-        float getYExtent = getExtents().getY();
-        Spatial tempSpatial = spatial;
-        tempSpatial.move(0, -getYExtent, 0);
-        spatial = new Node("enemy");
-        ((Node) spatial).attachChild(tempSpatial);
-
-        Node tempNode = (Node) spatial;
-        tempNode.move(0, getYExtent, 0);
-        addPhysicsControl();
-
-        spatial = new Node("enemy");
-        ((Node) spatial).attachChild(tempNode);
     }
 
     @Override
@@ -51,11 +35,7 @@ public class Bull extends MovableEntity {
 
     @Override
     public void cleanup() {
-        //bulletAppState.getPhysicsSpace().remove(characterControl);
-        //spatial.removeControl(characterControl);
-
         spatial.getControl(AggroControl.class).cleanup();
-        //spatial.removeControl(AggroControl.class);
     }
 
     @Override
