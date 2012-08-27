f3ae710a5d3d90f1134923c4a53b6b4b83bfdd16
terryr.rohr@gmail.com <terryr.rohr@gmail.com@1719d32a-b489-8db3-6133-feb022d4e5f0>
2012-08-27 21:11:47 +0000

    added screenshots

--- trunk/src/com/teamjmonkey/sound/MonkeySound.java
+++ trunk/src/com/teamjmonkey/sound/MonkeySound.java
@@ -2,8 +2,8 @@ package com.teamjmonkey.sound;
 
 public enum MonkeySound {
 
-        TEST_MUSIC("ahem.ogg", true),
-        TEST_SOUND("ahem.ogg", false),
+        TEST_MUSIC("click.ogg", true),
+        TEST_SOUND("click.ogg", false),
         OCEAN("Environment/Ocean Waves.ogg", true);
 
         private final String path;
